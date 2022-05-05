68584f5b0bab4b2ccb8511fe9f05fa276ec92400
JanAntonioTablizo <janantoniotablizo@gmail.com>
2022-05-05 15:30:54 +0800

    commit

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -6,19 +6,19 @@ const HomeContent = ()=>{
     return(
         <main id="home-main">
             <div id="movie-statuses">
-                <h1 id="nr-title">New Released Movies</h1>
+                <h1 className="mov-stat-titles" id="nr-mov">New Released Movies</h1>
                 <div className="movies custom-scrollbar" id="new-release">
                     {NRMovies.map((movie, key)=>(
                         <img key={key} src={movie["movie-img"]} alt={movie.title} className="nr-movies"/>
                     ))}
                 </div>
-                <h1 id="trend-title">Trending Movies</h1>
+                <h1 className="mov-stat-titles" id="trend-mov">Trending Movies</h1>
                 <div className="movies custom-scrollbar" id="trends">
                     {TrendMovies.map((movie, key)=>(
                         <img key={key} src={movie["movie-img"]} alt={movie.title} className="nr-movies"/>
                     ))}                    
                 </div>
-                <h1 id="cont-watch-title">Continue Watch Movies</h1>
+                <h1 className="mov-stat-titles" id="cont-mov">Continue Watch Movies</h1>
                 <div className="movies custom-scrollbar" id="continue-watch">
                     {ContWatchMovies.map((movie, key)=>(
                         <img key={key} src={movie["movie-img"]} alt={movie.title} className="nr-movies"/>
